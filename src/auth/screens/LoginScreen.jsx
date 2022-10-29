e64dde19ff8ab5407ac8e1a79d707657ce0cb410
David Alexander Enamorado Aceituno <daenamorado@bancatlan.hn>
2022-10-29 03:14:02 -0600

    add: desing del login

--- src/auth/screens/LoginScreen.jsx
+++ src/auth/screens/LoginScreen.jsx
@@ -1,9 +1,45 @@
-import { Typography } from "@mui/material"
+import { Grid, TextField, Typography } from "@mui/material"
 
 export const LoginScreen = () => {
   return (
-    <>
-      <Typography variant='h1'>Login</Typography>
-    </>
+    <Grid
+      container
+      spacing={0}
+      direction='column'
+      alignItems='center'
+      justifyContent='center'
+      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
+    >
+
+      <Grid item
+        className="box-shadow"
+        xs={3}
+        sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}
+      >
+        <Typography variant='h5' sx={{ mb: 1 }}>Login</Typography>
+        <form >
+
+          <Grid container>
+            <Grid item xs={12} sx={{mt: 2}}>
+              <TextField 
+              label='Correo' 
+              type='email' 
+              placeholder="ejemplo@gmail.com"
+              fullWidth 
+              />
+            </Grid>
+            <Grid item xs={12} sx={{mt: 2}}>
+              <TextField 
+              label='Contraseña' 
+              type='password' 
+              placeholder="Contraseña"
+              fullWidth 
+              />
+            </Grid>
+          </Grid>
+        </form>
+      </Grid>
+
+    </Grid>
   )
 }
