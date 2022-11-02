7fef656419b474b844c3aa22aa1affc96811d070
David Alexander Enamorado Aceituno <daenamorado@bancatlan.hn>
2022-11-01 22:36:32 -0600

    add: configuracion de redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
+import './styles.css'
+import { App } from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store/store'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { App } from './App'
-import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
