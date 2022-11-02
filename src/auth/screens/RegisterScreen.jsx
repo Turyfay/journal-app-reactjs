c730d78e9ec27e575599c322bdd8c5b0816ae857
David Alexander Enamorado Aceituno <daenamorado@bancatlan.hn>
2022-11-02 17:41:38 -0600

    add: uso del useForm en el formulario de registro

--- src/auth/screens/RegisterScreen.jsx
+++ src/auth/screens/RegisterScreen.jsx
@@ -1,11 +1,26 @@
 import { Button, Grid, Link, TextField, Typography } from "@mui/material"
 import { Link as RouterLink } from 'react-router-dom';
+import { useForm } from "../../hooks/useForm";
 import { LayoutAuth } from "../layout/LayoutAuth"
 
 export const RegisterScreen = () => {
+
+  const { nombres, apellidos, email, password, passwordRepeat, onInputChange, onResetForm, formState } = useForm({
+    nombres: '',
+    apellidos: '',
+    email: '',
+    password: '',
+    passwordRepeat: ''
+  });
+
+  const onSubmitRegister = (event) => {
+    event.preventDefault();
+    console.log(formState);
+  }
+
   return (
     <LayoutAuth title='Registro de Usuario'>
-      <form >
+      <form onSubmit={onSubmitRegister}>
 
         <Grid container>
           <Grid item xs={12} sx={{ mt: 3 }}>
@@ -14,6 +29,9 @@ export const RegisterScreen = () => {
               type='text'
               fullWidth
               size='small'
+              name="nombres"
+              value={nombres}
+              onChange={onInputChange}
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 3 }}>
@@ -22,6 +40,9 @@ export const RegisterScreen = () => {
               type='text'
               fullWidth
               size='small'
+              name='apellidos'
+              value={apellidos}
+              onChange={onInputChange}
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 3 }}>
@@ -31,6 +52,9 @@ export const RegisterScreen = () => {
               placeholder="ejemplo@gmail.com"
               fullWidth
               size='small'
+              name="email"
+              value={email}
+              onChange={onInputChange}
             />
           </Grid>
 
@@ -42,6 +66,9 @@ export const RegisterScreen = () => {
               placeholder="Contraseña"
               fullWidth
               size='small'
+              name="password"
+              value={password}
+              onChange={onInputChange}
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 3 }}>
@@ -51,20 +78,23 @@ export const RegisterScreen = () => {
               placeholder="Repetir Contraseña"
               fullWidth
               size='small'
+              name="passwordRepeat"
+              value={passwordRepeat}
+              onChange={onInputChange}
             />
           </Grid>
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }} >
 
             <Grid item xs={12} >
-              <Button variant="contained" fullWidth>
+              <Button type="submit" variant="contained" fullWidth>
                 Crear cuenta
               </Button>
             </Grid>
 
           </Grid>
           <Grid container direction='row' justifyContent='end'>
-            <Typography sx={{mr: 1}} >¿Ya tienes cuenta?</Typography>
+            <Typography sx={{ mr: 1 }} >¿Ya tienes cuenta?</Typography>
             <Link component={RouterLink} color='inherit' to='/auth/login'>
               Ingresar
             </Link>
