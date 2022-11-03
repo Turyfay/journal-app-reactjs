c808d9aa71c89066b26e8dc61da9c24f0d62b12b
David Alexander Enamorado Aceituno <daenamorado@bancatlan.hn>
2022-11-03 16:02:45 -0600

    add: custom hook authenticated

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,32 +1,16 @@
-import { onAuthStateChanged } from 'firebase/auth';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { AuthRoutes } from '../auth';
 import { CheckingAuth } from '../components';
-import { FirebaseAuth } from '../firebase/config';
+import { useCheckAuth } from '../hooks';
 import { JournalRoutes } from '../journal';
-import { login, logout } from '../store/auth';
 
 export const AppRouter = () => {
 
-  const { status } = useSelector(store => store.auth);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    onAuthStateChanged(FirebaseAuth, async (user) => {
-      console.log(user);
-      if (!user) return dispatch(logout());
-      const { uid, email, displayName, photoURL } = user;
-      dispatch(login({ uid, email, displayName, photoURL }));
-    })
-  }, []);
+  const { status } =  useCheckAuth();
 
   if (status === 'checking') {
     return <CheckingAuth />
   }
-
-
 
   return (
     <Routes>
