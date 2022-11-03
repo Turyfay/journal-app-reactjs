dfd8a229dd04ee22f5c8dbe682fd3e83edd355af
David Alexander Enamorado Aceituno <daenamorado@bancatlan.hn>
2022-11-03 15:46:01 -0600

    add: mantener el estado al recargar

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -1,15 +1,20 @@
+
 import { Navigate, Route, Routes } from "react-router-dom"
 import { LoginScreen } from "../screens/LoginScreen"
 import { RegisterScreen } from "../screens/RegisterScreen"
 
 export const AuthRoutes = () => {
+
+ 
+
+
   return (
     <Routes>
-        <Route path="login" element={<LoginScreen/>}/>
-        <Route path="register" element={<RegisterScreen/>}/>
+      <Route path="login" element={<LoginScreen />} />
+      <Route path="register" element={<RegisterScreen />} />
 
 
-        <Route path="/*" element={<Navigate to='/auth/login'/>}/>
+      <Route path="/*" element={<Navigate to='/auth/login' />} />
     </Routes>
   )
 }
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,48 @@
-import { Route, Routes } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { AuthRoutes } from '../auth';
+import { CheckingAuth } from '../components';
+import { FirebaseAuth } from '../firebase/config';
 import { JournalRoutes } from '../journal';
+import { login, logout } from '../store/auth';
 
 export const AppRouter = () => {
+
+  const { status } = useSelector(store => store.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(FirebaseAuth, async (user) => {
+      console.log(user);
+      if (!user) return dispatch(logout());
+      const { uid, email, displayName, photoURL } = user;
+      dispatch(login({ uid, email, displayName, photoURL }));
+    })
+  }, []);
+
+  if (status === 'checking') {
+    return <CheckingAuth />
+  }
+
+
+
   return (
     <Routes>
+
+      {
+        status === 'authenticated'
+          ? <Route path='/*' element={<JournalRoutes />} />
+          : <Route path='/auth/*' element={<AuthRoutes />} />
+      }
+
+      <Route path='/*' element={<Navigate to='/auth/login' />}/>
+
       {/* Login and register */}
-      <Route path='/auth/*' element={<AuthRoutes />} />
+      {/*  <Route path='/auth/*' element={<AuthRoutes />} /> */}
       {/* Journal Routes  */}
-      <Route path='/*' element={<JournalRoutes />} />
+      {/*  <Route path='/*' element={<JournalRoutes />} /> */}
 
     </Routes>
   )
